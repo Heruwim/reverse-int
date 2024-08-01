d84a05bbdc377a77fc1aad08482ec518b9a59b92
Heruwim <heruwim25@gmail.com>
2024-08-01 12:12:11 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const nubmerToString = Math.abs(n).toString();
+    let reverseNumber = "";
+    for (let i = nubmerToString.length - 1; i >= 0; i--) {
+        reverseNumber += nubmerToString[i];
+    }
+    return Number(reverseNumber);
 }
